3198f4230325d1eac8d9149b1209256ceba8dfc2
chenshuiluke <chenshuiluke@gmail.com>
2016-01-30 21:55:06 -0500

    Added basic UI layout. No real functionality yet

---
+++ src/Notey.java
@@ -0,0 +1,39 @@
+package notey;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import java.net.URL;
+public class Notey extends Application{
+	public static void main(String[] args){
+		Application.launch(args);
+	}
+	public void start(Stage primaryStage) throws IOException{
+			Parent root = null;
+			String sceneFile = "main.fxml";
+	    URL    url  = null;
+	    try
+	    {
+				url  = getClass().getClassLoader().getResource( sceneFile );
+				root = FXMLLoader.load( url );
+				System.out.println( "  fxmlResource = " + sceneFile );
+				Scene scene = new Scene(root);
+				primaryStage.setScene(scene);
+				primaryStage.show();
+	    }
+	    catch ( Exception ex )
+	    {
+				System.out.println( "Exception on FXMLLoader.load()" );
+				System.out.println( "  * url: " + url );
+				System.out.println( "  * " + ex );
+				System.out.println( "    ----------------------------------------\n" );
+				throw ex;
+	    }
+
+	}
+}
---
+++ src/NoteyController.java
@@ -0,0 +1,40 @@
+package notey;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import java.net.URL;
+import javafx.fxml.FXML;
+import javax.annotation.Resources;
+public class NoteyController{
+
+  private boolean showSidePane;
+  private double dividerPosition;
+  @FXML
+	private SplitPane mainViewPane;
+  private AnchorPane sidePane;
+
+  public void sidePaneVisibility(ActionEvent event){
+  		if(!showSidePane){
+        dividerPosition = mainViewPane.getDividerPositions()[0];
+        mainViewPane.setDividerPositions(0.0);
+        showSidePane = true;
+      }
+      else{
+        mainViewPane.setDividerPositions(dividerPosition);
+        showSidePane = false;
+      }
+	}
+  public void initialize(URL location, Resources resources){
+    showSidePane = true;
+    System.out.println("initialized");
+  }
+}
